91308bd7030df7e8eaeb87a3f162216aff0f2a6f
cms1598 <cms1598@gmail.com>
2022-08-08 00:59:15 -0400

    trying to connect

--- RYD.py
+++ RYD.py
@@ -20,5 +20,5 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('MTAwNDU0NjI3MTU4OTE3NTM2OA.GmTHxO.0PpRqxa1cDxUYRqqa41qJw-DeQlp25CAYr-QCc')
+client.run('MTAwNjA2MzcyNTM1NjI2NTUyMg.GaAs6M.nXl3_cVJqLeVvVX_7l_1vX8i_DMGhMxgFNz7Pk')
 # ssh -i C:\Users\crist\Desktop\RYD\RYD-keypair.pem ubuntu@ec2-3-95-207-0.compute-1.amazonaws.com
