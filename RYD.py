2297645635ea8de5a98fcb6707cb7b7a296294b7
cms1598 <cms1598@gmail.com>
2022-08-08 00:26:30 -0400

    trying to connect

--- RYD.py
+++ RYD.py
@@ -14,4 +14,4 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 
-client.run('MTAwNDU0NjI3MTU4OTE3NTM2OA.Go3Ebo.Jp90AwpUPCTPwMXoTPBHE29YonirY71xqoXvg0')
+client.run('MTAwNDU0NjI3MTU4OTE3NTM2OA.GT7dcd.LVHaG0llpXhpVPjfhsQdydl79aQI-6vNZfsWRs')
