ad30f7cfce4a3516e84aad5b5153a0afc1173638
cms1598 <cms1598@gmail.com>
2022-08-08 00:52:34 -0400

    trying to connect

--- RYD.py
+++ RYD.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 # TOKEN = os.getenv('DISCORD_TOKEN')[1:-1]
-TOKEN = "MTAwNDU0NjI3MTU4OTE3NTM2OA.GdNQ4G.-4juxLTTu9esTO0oPdg9g5HyyCnYcrmFTc33Tk"
+TOKEN = "MTAwNDU0NjI3MTU4OTE3NTM2OA.GFIdmw.81NLtLv42_gLWUUhOlrlLzaJHvCl7Iz9T8r6n0"
 
 client = discord.Client()
 
@@ -15,7 +15,7 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-print(TOKEN)
+# print(TOKEN)
 # ssh -i C:\Users\crist\Desktop\RYD\RYD-keypair.pem ubuntu@ec2-3-95-207-0.compute-1.amazonaws.com
 
 client.run(TOKEN)
